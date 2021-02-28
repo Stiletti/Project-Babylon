dfc521cc455556d8d99ff57768b287cc4b1d5fc6
DESKTOP-GEB2BJ8\stiev <stievvus@yahoo.de>
2021-02-28 10:30:20 +0100

    Basic Enemy Setup created

---
+++ Source/MyProject/EnemyAnimInstance.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EnemyAnimInstance.h"
+#include "Enemy.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+void UEnemyAnimInstance::NativeInitializeAnimation()
+{
+	if (Pawn == nullptr)
+	{
+		Pawn = TryGetPawnOwner();
+
+		if (Pawn)
+		{
+			Enemy = Cast<AEnemy>(Pawn);
+		}
+	}
+}
+
+void UEnemyAnimInstance::UpdateAnimProps()
+{
+	if (Pawn == nullptr)
+	{
+		Pawn = TryGetPawnOwner();
+
+		if (Pawn)
+		{
+			FVector Speed = Pawn->GetVelocity();
+			FVector LateralSpeed = FVector(Speed.X, Speed.Y, 0.0f); // LateralSpeed just take care for the Yawvalues
+			MovementSpeed = LateralSpeed.Size();
+
+			Enemy = Cast<AEnemy>(Pawn);
+		}
+	}
+}
---
+++ Source/MyProject/EnemyAnimInstance.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "EnemyAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT_API UEnemyAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	
+	virtual void NativeInitializeAnimation() override; // its like BeginPlay() just for Animation Blueprints
+
+	UFUNCTION(BlueprintCallable, Category = "Movement")
+		void UpdateAnimProps();
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement")
+		float MovementSpeed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement")
+		class APawn* Pawn;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Movement")
+		class AEnemy* Enemy;
+};
